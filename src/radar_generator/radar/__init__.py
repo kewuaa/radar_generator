cbd94345cbfb03623154d89ee14169a0d88f0cf9
kewuaa <202321011111@std.uestc.edu.cn>
2025-03-13 14:09:31 +0800

    remove api related

--- src/radar_generator/radar/__init__.py
+++ src/radar_generator/radar/__init__.py
@@ -1,14 +1,6 @@
 import heapq
-from pathlib import Path
 from typing import Iterator
 
-import tomli
-
-from .doa import DOA
-from .pa import PA
-from .pri import PRI
-from .pw import PW
-from .rf import RF
 from .types import PDW, Radar
 
 
@@ -39,53 +31,5 @@ class Generator:
 
     def clear(self) -> None:
         self._radars.clear()
-    #enddef
-
-    @classmethod
-    def from_toml(cls, path: str | Path) -> "Generator":
-        path = Path(path).absolute()
-        if not path.exists():
-            raise RuntimeError(f"`{path}` not exists")
-        #endif
-        with open(path, "rb") as f:
-            config = tomli.load(f)
-        #endwith
-        if not isinstance(config, dict):
-            raise RuntimeError("load config failed")
-        #endif
-        radars: list[dict] | None = config.get("radar", None)
-        if radars is None:
-            raise RuntimeError("`radar` field not found")
-        #endif
-
-        generator = cls()
-        for i, radar in enumerate(radars):
-            start: float = radar.get("start_toa", 0)
-            pri_config: dict | None = radar.get("pri")
-            doa_config: dict | None = radar.get("doa")
-            rf_config: dict | None = radar.get("rf")
-            pw_config: dict | None = radar.get("pw")
-            if (
-                pri_config is None
-                or doa_config is None
-                or rf_config is None
-                or pw_config is None
-            ):
-                raise RuntimeError("loss radar field")
-            #endif
-            generator._radars.append(
-                Radar(
-                    i,
-                    start,
-                    PRI(**pri_config),
-                    DOA(**doa_config),
-                    RF(**rf_config),
-                    PW(**pw_config),
-                    PA(),
-                    radar.get("loss_rate")
-                )
-            )
-        #endfor
-        return generator
     #enddef
 #endclass
